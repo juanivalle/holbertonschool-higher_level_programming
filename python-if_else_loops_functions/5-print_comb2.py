d21859b2c3b0f741f3357133b93b32dc4ec5b205
juanivalle <juanignaciovalle18@gmail.com>
2023-01-17 08:28:32 -0800

    task5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for j in range(0, 100):
     if j < 99:
-        print("{num:02d}".format(num = j), end=", ")
+        print("{num:02d}".format(num=j), end=", ")
     else:
         print("{}".format(j))
