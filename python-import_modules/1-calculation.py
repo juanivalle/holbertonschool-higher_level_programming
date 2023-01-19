85ad818714b44f9f4a4633bf7a0a48fbf4fb2c3a
juanivalle <juanignaciovalle18@gmail.com>
2023-01-19 06:00:56 -0800

    task2

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    c = add(a, b)
+    d = sub(a, b)
+    e = mul(a, b)
+    f = div(a, b)
+    print("{} + {} = {}".format(a, b, c))
+    print("{} - {} = {}".format(a, b, d))
+    print("{} * {} = {}".format(a, b, e))
+    print("{} / {} = {}".format(a, b, f))
