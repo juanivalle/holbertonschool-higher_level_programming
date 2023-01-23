1dc4506c7ce36122d6aab3a17bcff461d0820842
juanivalle <juanignaciovalle18@gmail.com>
2023-01-23 09:21:47 -0800

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-   for fila in (matrix):
-        for valor in fila:
-            print("{:d}".format(valor), end=" ")
-        print() 
+   for i in (matrix):
+        for n in i:
+            if n != matrix[-1]:
+                print("{}".format(n), end=" ")
+            else:
+                print("{}".format(n))
+        print()
