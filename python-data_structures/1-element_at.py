b3cff3d09603c3c101b56df3960ac4800cd72e66
juanivalle <juanignaciovalle18@gmail.com>
2023-01-23 07:16:54 -0800

    task1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in range(0, len(my_list)-1):
+    for i in range(len(my_list)-1):
         if idx < 0 and idx >= i:
             return(None)
         else:
