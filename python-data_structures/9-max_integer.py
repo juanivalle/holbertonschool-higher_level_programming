cce57eb1961ef60d990b807aa851b8b3862046aa
juanivalle <juanignaciovalle18@gmail.com>
2023-01-24 05:41:35 -0800

    task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return(None)
+    else:
+        maximo = min(my_list)
+        for i in my_list:
+            if i > maximo:
+                maximo = i
+        return(maximo)
+
+
