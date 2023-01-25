f7680500cfb5645aee0ceb4580094d7c85abcb9e
juanivalle <juanignaciovalle18@gmail.com>
2023-01-25 06:48:11 -0800

    task 4

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    new = []
+    new2 = []
+    for i in set_1:
+        for n in set_2:
+            if i == n:
+                continue
+            else:
+                new.append(i)
+                new.append(n)
+    new2 = set_1.intersection(set_2)
+    for a in new:
+        for b in new2:
+            if b == a:
+                new.remove(a)
+    return(set(new))
