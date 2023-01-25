d3f93902e0dbc94df786eb88b60ffe24dbde6271
juanivalle <juanignaciovalle18@gmail.com>
2023-01-25 05:59:33 -0800

    task 3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    new = []
     for i in set_1:
         for n in set_2:
             if i == n:
-                return(i)
+                new.append(i)
+    return(set(new))
