ebfbdd59ba01ae67c0e978e7d301da0d2722f077
juanivalle <juanignaciovalle18@gmail.com>
2023-01-25 08:03:50 -0800

    task 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    dicc_sorted = sorted(a_dictionary.items())
-    print("{}".format(dicc_sorted))
+    keys = list(a_dictionary.keys())
+    keys.sort()
+    dicc_sorted = {i: a_dictionary[i] for i in keys}
+    for key, value in dicc_sorted.items():
+        print("{}: {}".format(key, value))
