c70e362d18b63caebe0293343f61410a9a64ee23
juanivalle <juanignaciovalle18@gmail.com>
2023-01-26 07:33:05 -0800

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary == None:
         return(None)
+    elif not a_dictionary:
+        return(None)
     else:
-        key_max = max(a_dictionary, key = a_dictionary.get)
+        key_max = max(a_dictionary, key=a_dictionary.get)
     return(key_max)
