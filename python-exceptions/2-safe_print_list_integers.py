285afadd05c9e21092cddd328b1af8cc0de08171
juanivalle <juanignaciovalle18@gmail.com>
2023-01-27 07:38:57 -0800

    task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    count = 0
+    try:
+        while i < x:
+            if isinstance(my_list[i], int):
+                print(my_list[i], end="")
+                count += 1
+            i += 1 
+    except IndexError:
+        pass
+    print("")
+    return count
