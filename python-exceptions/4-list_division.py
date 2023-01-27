e66d655756065dd5edcde97207119d9d0ec3bf7a
juanivalle <juanignaciovalle18@gmail.com>
2023-01-27 09:14:25 -0800

    task 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -8,7 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
             new.append(0)
             continue
-        except (ValueError,TypeError):
+        except (ValueError, TypeError):
             print("wrong type")
             new.append(0)
             continue
