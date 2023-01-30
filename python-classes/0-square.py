48a335bc165c5498e3e2de3a76667d89ac72f90c
juanivalle <juanignaciovalle18@gmail.com>
2023-01-30 07:43:59 -0800

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#Write an empty class Square that defines a square#
+"""Write an empty class Square that defines a square"""
 class Square:
-    #defines a square#
+    """defines a square"""
     pass
