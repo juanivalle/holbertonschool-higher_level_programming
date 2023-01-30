c219079d02012d0968da192b5fa00ac2f3796eb1
juanivalle <juanignaciovalle18@gmail.com>
2023-01-30 08:06:47 -0800

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """define a square"""
+
+
+    def __init__(self, size=0):
+        """init"""
+        pass
