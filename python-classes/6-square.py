ccba021d897c9858272e0c034b2ab24eb36dd892
juanivalle <juanignaciovalle18@gmail.com>
2023-01-30 10:43:48 -0800

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -32,8 +32,8 @@ class Square:
     @position.setter
     def position(self, value):
         self.__position = value
-        for i in (value):
-            if i < 0:
+        for i in (self.__position):
+            if not isinstance(self.__position[i], int):
                 raise TypeError("positionmustbeatuple of 2 positive integers")
 
     def area(self):
