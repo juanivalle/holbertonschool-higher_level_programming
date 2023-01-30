51d21ff21e74de57e9f2d616ea32cb7b124014ad
juanivalle <juanignaciovalle18@gmail.com>
2023-01-30 10:59:47 -0800

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -59,7 +59,7 @@ class Square:
         else:
             if self.__position[1] > 0:
                 print()
-                print(end="")
+                print(end="" * self.__position[1])
             for i in range(self.__size):
                 print(" " * self.__position[0], end="")
                 print("#" * self.__size)
