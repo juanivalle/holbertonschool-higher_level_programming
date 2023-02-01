f4b25930624591ef019f30f472004c3b61915c8f
juanivalle <juanignaciovalle18@gmail.com>
2023-02-01 07:25:52 -0800

    task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,7 +3,7 @@
 
 
 def say_my_name(first_name, last_name=""):
-     """Function that prints 'My name is {first_name} {last_name}'"""
+    """Function that prints 'My name is {first_name} {last_name}'"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
