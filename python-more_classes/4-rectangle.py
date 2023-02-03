df488bc92773313152a8908cca3ac2ae8f6b0660
juanivalle <juanignaciovalle18@gmail.com>
2023-02-03 06:18:18 -0800

    task 4

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Write a class rectangel that defines a square"""
+
+
+class Rectangle:
+    """defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """method that returns the square area"""
+        area = self.__height * self.__width
+        return(area)
+
+    def perimeter(self):
+        """method that returns the square area"""
+        if self.__height == 0 or self.__width == 0:
+            perimeter = 0
+        else:
+            perimeter = (self.__height + self.__width) * 2
+        return(perimeter)
+
+    def __str__(self):
+        rec = ""
+        if self.__height is 0 or self.__width is 0:
+            return rec
+        else:
+            for i in range(self.__height):
+                rec += ("#" * self.__width)
+                if (i < self.__height - 1):
+                    rec += ("\n")
+        return rec
+
+    def __repr__(self):
+        return(f"Rectangle({self.__width:d}, {self.__height:d})")
