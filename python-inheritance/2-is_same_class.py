311ed2a92f33cce2f271e81a173e41425245e078
juanivalle <juanignaciovalle18@gmail.com>
2023-02-07 07:10:38 -0800

    task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class MyList that inherits from list"""
+
+
+def is_same_class(obj, a_class):
+    """function"""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
