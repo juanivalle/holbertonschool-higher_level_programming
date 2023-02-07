e21ef8fd480d599ed02a5529a3b0983cac7e5863
juanivalle <juanignaciovalle18@gmail.com>
2023-02-07 06:40:13 -0800

    task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""function that returns the list of available attributes and methods of an object"""
+"""returns the list of available attributes and methods of an object"""
 
 
 def lookup(obj):
     """function"""
-    
+
     return(dir(obj))
