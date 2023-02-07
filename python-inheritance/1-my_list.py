f73dcace1a8eee053cf1bd9cde6955e6a19facc3
juanivalle <juanignaciovalle18@gmail.com>
2023-02-07 07:01:57 -0800

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class MyList that inherits from list"""
+
+
+class MyList(list):
+    """class"""
+
+    def print_sorted(self):
+        """function"""
+
+        print(sorted(self))
