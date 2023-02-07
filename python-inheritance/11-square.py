4970eba22738ef33e93c78eecbb6174b0cda66ba
juanivalle <juanignaciovalle18@gmail.com>
2023-02-07 09:00:19 -0800

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class"""
+
+    def __init__(self, size):
+        """initialization"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """area"""
+
+        return(self.__size ** 2)
+
+    def __str__(self):
+        return("[Square] " + str(self.__size) + "/" + str(self.__size))
