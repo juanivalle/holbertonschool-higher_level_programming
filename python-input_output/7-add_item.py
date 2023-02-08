0d6256346868c3665455d8c770437c2aab45d613
juanivalle <juanignaciovalle18@gmail.com>
2023-02-08 07:14:24 -0800

    task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""comments"""
+
+
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+filename = "add_item.json"
+
+try:
+    py_list = load_from_json_file(filename)
+
+except FileNotFoundError:
+    py_list = []
+    for i in range(1, len(sys.argv)):
+        py_list.append(sys.argv[i])
+    save_to_json_file(_list, filename)
