e7e23ecb54fdb9366f0adefa06487a6a784e33a8
juanivalle <juanignaciovalle18@gmail.com>
2023-02-08 08:11:00 -0800

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""comments"""
+
+
+class Student:
+    """comments"""
+
+    def __init__(self, first_name, last_name, age):
+        """comments"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """comments"""
+
+        if attrs is None:
+            return self.__dict__
+        if all(isinstance(i, str) for i in attrs):
+            if isinstance(attrs, list):
+                return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
