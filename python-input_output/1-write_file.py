e8277196e0d93946fe79b44f04849307d703abed
juanivalle <juanignaciovalle18@gmail.com>
2023-02-08 06:30:07 -0800

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,9 @@
 def write_file(filename="", text=""):
     """function that write"""
 
+    a = 0
     with open(filename, mode="r", encoding="utf-8") as f:
-        with open(filename, mode="w", encoding="utf-8") as f:
-            f.write(text)
+        for i in f:
+            for n in i:
+                a = a + 1
+        return a
