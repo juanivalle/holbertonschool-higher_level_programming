2b736b49c0f5399afb6b908a7c0d5669da7dd3df
juanivalle <juanignaciovalle18@gmail.com>
2023-02-08 07:43:31 -0800

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""load, add, save"""
+"""Load, add, save"""
 
 
 import sys
@@ -7,12 +7,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
-objeto = [arg for arg in sys.argv if arg != sys.argv[0]]
 try:
     py_list = load_from_json_file(filename)
 except FileNotFoundError:
-    save_to_json_file(objeto, filename)
-else:
-    for i in objeto:
-        py_lsit.append(i)
-    save_to_json_file(py_list, filename)
+    py_list = []
+for i in range(1, len(sys.argv)):
+    py_list.append(sys.argv[i])
+save_to_json_file(py_list, filename)
