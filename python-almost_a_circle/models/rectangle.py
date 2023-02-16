728f03ea8a9ba4e9c0b26175988a73470c3377b7
juanivalle <juanignaciovalle18@gmail.com>
2023-02-16 07:23:57 -0800

    task 4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -64,3 +64,7 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
+
+    def area(self):
+        area = self.__width * self.__height
+        return(area)
