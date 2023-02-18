757b95a9eddb8a30e81ab5e8f5c51f240352061f
juanivalle <juanignaciovalle18@gmail.com>
2023-02-18 12:38:56 -0800

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,7 +2,7 @@
 """
 comments
 """
-
+import json
 
 class Base:
     """
@@ -20,3 +20,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return
+        else:
+            return json.dumps(list_dictionaries)
