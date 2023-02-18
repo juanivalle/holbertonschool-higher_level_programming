b1484682657484b3c197d23cd6e5f651c3a5fce8
juanivalle <juanignaciovalle18@gmail.com>
2023-02-18 13:01:18 -0800

    task 16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -29,4 +29,18 @@ class Base:
         if list_dictionaries is None:
             return("[]")
         else:
-            return json.dumps(list_dictionaries)
+            return json.dumps(list_dictionaries)   
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """comments"""
+
+        if list_objs is None:
+            new_list = []
+        else:
+            new_list = [cls.to_dictionary(obj) for obj in list_objs]
+
+        filename = cls.__name__ + ".json"
+
+        with open(filename, mode="w") as file:
+            file.write(cls.to_json_string(new_list))
