a0b84beac03897fa1a53a6062236f3ec71a92a9f
juanivalle <juanignaciovalle18@gmail.com>
2023-03-15 10:46:25 -0300

    fix task 4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,3 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-CREATE TABLE IF NOT EXISTS first_table(
-    id INT
-    NAME VARCHAR(256)
-)
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256))
