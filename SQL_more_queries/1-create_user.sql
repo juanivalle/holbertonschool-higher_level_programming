41b78fa14da9922cd6990074460fba2e1c0b0946
juanivalle <juanignaciovalle18@gmail.com>
2023-03-15 12:23:33 -0300

    task 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE IF NOT EXISTS LOGIN user_0d_1 WITH PASSWORD = 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1 '@'localhost';
