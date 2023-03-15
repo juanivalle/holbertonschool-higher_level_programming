61cedcddf73114f53d292913a193711e06bab203
juanivalle <juanignaciovalle18@gmail.com>
2023-03-15 10:40:08 -0300

    add comments

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
--- SQL proyect
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,2 +1,3 @@
--- SQL proyect
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
 SHOW TABLES;
