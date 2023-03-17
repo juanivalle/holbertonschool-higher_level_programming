0420a98b2d4da1efe8c4bcc9eebd3f072cf057f9
juanivalle <juanignaciovalle18@gmail.com>
2023-03-17 12:25:08 -0300

    task 14

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT sh.name
+FROM tv_genres s JOIN tv_show_genres tv
+ON
+    sh.id = tv.genre_id JOIN tv_shows ge ON ge.id = tv.show_id
+WHERE ge.title = 'Dexter'
+ORDER BY sh.name;
