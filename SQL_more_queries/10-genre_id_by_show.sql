1ce8428d43dc1c15dd798fb54b5a9f5645ab9176
juanivalle <juanignaciovalle18@gmail.com>
2023-03-17 11:28:25 -0300

    task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,8 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT show.title, show_ge.genre_id
-FROM tv_shows show INNER JOIN tv_show_genres show_ge
-ON
-	show.id = show_ge.show_id
-ORDER BY show.title, show_ge.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON 
+	tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
