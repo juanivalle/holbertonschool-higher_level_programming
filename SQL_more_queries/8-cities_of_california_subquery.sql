dfe74a474dba5487cd50d817e27ebec5c3d55942
juanivalle <juanignaciovalle18@gmail.com>
2023-03-17 10:40:57 -0300

    task 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT * FROM hbtn_0d_usa.states, hbtn_0d_usa.cities WHERE name = california ORDER BY cities ASC;
+SELECT * FROM states, cities WHERE name = california ORDER BY cities ASC;
