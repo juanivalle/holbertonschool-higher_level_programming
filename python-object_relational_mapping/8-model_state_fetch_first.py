f786e904d708f21d0630db501d8a988875947a1b
juanivalle <juanignaciovalle18@gmail.com>
2023-03-19 11:21:49 -0700

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""task 8 print first state"""
-if __name__ == '__main__':
-    from sys import argv
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    from model_state import Base, State
-    from sqlalchemy import select
+""" task 8 """
 
-    mysql_username, mysql_password, database_name = argv[1], argv[2], argv[3]
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == '__main__':
+   mysql_username, mysql_password, database_name = argv[1], argv[2], argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        mysql_username, mysql_password, database_name), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+        mysql_username, mysql_password, database_name))
 
-    with Session(engine) as session:
-        query = select(State).order_by(State.id)
-        exe = session.execute(query).first()
-        if exe is not None:
-            print("{}: {}".format(exe[0].id, exe[0].name))
-        else:
-            print("Nothing")
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(first_state.id, first_state.name))
