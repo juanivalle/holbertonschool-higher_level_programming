a58c058ce604781b666aa1e705de21cf1c9bd56f
juanivalle <juanignaciovalle18@gmail.com>
2023-03-19 08:11:41 -0700

    task 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" task 6 """
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer(), primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
