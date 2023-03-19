7d81847231080f466c288a60cbe42005143770c5
juanivalle <juanignaciovalle18@gmail.com>
2023-03-19 11:23:47 -0700

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ task 8 """
-
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
 if __name__ == '__main__':
-   mysql_username, mysql_password, database_name = argv[1], argv[2], argv[3]
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    
+    mysql_username, mysql_password, database_name = argv[1], argv[2], argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         mysql_username, mysql_password, database_name))
