1339d48dc8c86431079e9d5608f20d635cc7adf2
juanivalle <juanignaciovalle18@gmail.com>
2023-03-19 07:29:51 -0700

    task 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    task 3
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    arg = sys.argv
+    db = MySQLdb.connect(host=host="localhost", port=3306, 
+            user=arg[1],passwd=arg[2], db=arg[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.name FROM cities LEFT JOIN states ON cities.state_id = states.id WHERE states.name=%s ORDER BY cities.id ASC".format(arg[4]))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
