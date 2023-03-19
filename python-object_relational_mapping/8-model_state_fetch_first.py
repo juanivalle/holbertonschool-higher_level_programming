c1a4ed3f2332c614f4a0649e0495d2208d8fccd8
juanivalle <juanignaciovalle18@gmail.com>
2023-03-19 11:18:15 -0700

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,19 +5,19 @@ if __name__ == '__main__':
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
     from model_state import Base, State
+    from sqlalchemy import select
 
     mysql_username, mysql_password, database_name = argv[1], argv[2], argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        mysql_username, mysql_password, database_name))
+        mysql_username, mysql_password, database_name), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
+    with Session(engine) as session:
+        query = select(State).order_by(State.id)
+        exe = session.execute(query).first()
+        if exe is not None:
+            print("{}: {}".format(exe[0].id, exe[0].name))
+        else:
+            print("Nothing")
 
-    session = Session()
-
-    first_state = session.query(State).order_by(State.id).first()
-
-    if first_state is None:
-        print("Nothing")
-    else:
-        print("{}: {}".format(first_state.id, first_state.name))
