1f0ae3bf1e50d951c7bfc9d6aa204c1801a7321e
juanivalle <juanignaciovalle18@gmail.com>
2023-03-19 07:17:53 -0700

    task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    task 3
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    arg = sys.argv
+    db = MySQLdb.connect(host=host="localhost", port=3306, 
+            user=arg[1],passwd=arg[2], db=arg[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC".format(arg[4]))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
