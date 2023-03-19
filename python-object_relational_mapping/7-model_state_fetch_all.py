ab7b912bfabf3603c834aa75b6515de4378b9ef1
juanivalle <juanignaciovalle18@gmail.com>
2023-03-19 08:38:25 -0700

    task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
     Base.metadata.create_all(engine)
     
-    Session = sessionmaker(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     states = session.query(State).order_by(State.id).all()
