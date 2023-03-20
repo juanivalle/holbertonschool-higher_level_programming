7926ee918dc175b7160136d63298985ff48f9277
juanivalle <juanignaciovalle18@gmail.com>
2023-03-20 09:50:26 -0300

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,12 +2,12 @@
 """
 Task 0 get all states
 """
-if __name__ == '__main__':
-    import MySQLdb
-    import sys
+import MySQLdb
+import sys
 
+if __name__ == '__main__':
     arg = sys.argv
-    db = MySQLdb.connect(host=host="localhost", port=3306, 
+    db = MySQLdb.connect(host="localhost", port=3306, 
             user=arg[1],passwd=arg[2], db=arg[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
