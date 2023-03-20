e10cb7fd57c68ed7028e57ea635321fe39a532e0
juanivalle <juanignaciovalle18@gmail.com>
2023-03-20 10:40:20 -0300

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -14,7 +14,11 @@ if __name__ == "__main__":
                    FROM states JOIN cities ON state_id=states.id \
                    WHERE states.name LIKE %s ORDER BY cities.id",(arg[4],))
     rows = cursor.fetchall()
+    i = ""
     for row in rows:
-        print(row)
+        if (row != rows[0]):
+            i += ", "
+        i += row[0]
+    print(i)
     cursor.close()
     db.close()
