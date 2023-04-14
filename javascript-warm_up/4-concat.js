026573f0a3e72ce91afbf794a7f6219a16fedd13
juanivalle <juanignaciovalle18@gmail.com>
2023-04-14 12:44:42 -0300

    task 4

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,15 +3,4 @@
 const arg = process.argv[2];
 const arg2 = process.argv[3];
 
-let i = 0;
-while (i <= arg.length) {
-  if (i === 0) {
-    console.log(arg);
-  } else {
-    console.log(arg2);
-  }
-  if (i === 0) {
-    console.log('is');
-  }
-  i++;
-}
+console.log(arg + ' is ' + arg2);
