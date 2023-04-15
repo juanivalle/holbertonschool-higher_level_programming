a89c760892f5b4d2ce8ceaf7e688b61b8503267d
juanivalle <juanignaciovalle18@gmail.com>
2023-04-15 13:49:53 -0300

    task 3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,19 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (width, height) {
-      if (width <= 0 || height <= 0 || isNaN(width) || isNaN(height)) {
-        return undefined;
-      } else {
-        this.width = width;
-        this.height = height;
-      }
+  constructor (width, height) {
+    if (width <= 0 || height <= 0 || isNaN(width) || isNaN(height)) {
+      return undefined;
+    } else {
+      this.width = width;
+      this.height = height;
     }
-    print() {
-        for (let i = 0; i < this.height; i++) {
-            console.log('X' .repeat(this.width));
-        }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
+  }
 }
-  module.exports = Rectangle;
+module.exports = Rectangle;
