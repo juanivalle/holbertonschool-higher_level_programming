424a3798b4087741f56b75f2b160899687db0a98
juanivalle <juanignaciovalle18@gmail.com>
2023-04-19 11:58:50 -0300

    fix task 0 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,9 +5,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const text = process.argv[3];
 
-
 fs.writeFile(filePath, text, 'utf-8', (err) => {
-    if (err) {
-        console.error(err);
-      }
+  if (err) {
+    console.error(err);
+  }
 });
